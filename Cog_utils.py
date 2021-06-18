2ecddbaeac08c3420e4154c0bbdec18ab931e688
Visceros <propok63@gmail.com>
2021-06-18 16:50:16 +0300

    The user is now automatically added to database when he enters one of the channels that are used to account statistics.

--- Cog_utils.py
+++ Cog_utils.py
@@ -95,16 +95,18 @@ class Listeners(commands.Cog):
                channel_groups_to_account_contain):
             try:
                 gold = await db.fetchval(f'SELECT gold from discord_users WHERE id={member.id}')
-            except TypeError:
-                try:
-                    await db.execute(
-                        'INSERT INTO discord_users (id, nickname, join_date, gold, warns) VALUES($1, $2, $3, 0, 0);',
-                        member.id, member.display_name, member.joined_at)
-                    await sys_channel.send(f'user added to database: {member.display_name}')
-                except asyncpg.exceptions.UniqueViolationError:
-                    await sys_channel.send(f'user {member.display_name}, id: {member.id} is already added')
-                finally:
-                    await self.pool.release(db)
+                if type(gold) == 'NoneType' or gold is None:
+                    try:
+                        await db.execute(
+                            'INSERT INTO discord_users (id, nickname, join_date, gold, warns) VALUES($1, $2, $3, 0, 0);',
+                            member.id, member.display_name, member.joined_at)
+                        await sys_channel.send(f'user added to database: {member.display_name}')
+                    except asyncpg.exceptions.UniqueViolationError:
+                        await sys_channel.send(f'user {member.display_name}, id: {member.id} is already added')
+                    finally:
+                        await self.pool.release(db)
+            except asyncpg.connection.exceptions.ConnectionRejectionError or asyncpg.connection.exceptions.ConnectionFailureError:
+                db = self.pool.acquire()
 
         if str(member.status) not in ['invisible', 'dnd'] and not member.bot:
             if before.channel is None and after.channel is not None and not after.afk:
