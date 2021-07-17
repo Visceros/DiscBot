b27ab2e66b9c26cce1ca7ad5313ee70a2e95a298
Visceros <propok63@gmail.com>
2021-07-17 22:33:22 +0300

    Добавил декораторы @commands.Cog.listener к on_message, on_member_remove и on_member_update. Нужны тесты on_message, on_member_remove. on_member_update - работает корректно.

--- Cog_utils.py
+++ Cog_utils.py
@@ -172,11 +172,10 @@ class Listeners(commands.Cog):
     async def on_message(self, message:discord.Message):
         #guild = message.author.guild
         if not message.content.startswith('!'):
-            db = await self.pool.acquire()
-            print('message counted')
-            messages = await db.fetchval(f'SELECT messages FROM LogTable WHERE user_id={message.author.id}')
-            await db.execute(f'UPDATE LogTable SET messages={int(messages)+1} WHERE user_id={message.author.id} ORDER BY login DESC')
-            await self.pool.release(db)
+            async with self.pool.acquire() as con:
+                print('message counted')
+                messages = await db.fetchval(f'SELECT messages FROM LogTable WHERE user_id={message.author.id}')
+                await db.execute(f'UPDATE LogTable SET messages={int(messages)+1} WHERE user_id={message.author.id} ORDER BY login DESC')
         else:
             print('It is a command')
 
