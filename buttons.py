9fdf41ce78c15b08b9bc5c14bdb8a95854a23d58
Visceros <propok63@gmail.com>
2026-04-08 18:36:31 +0300

    Добавление кнопок под обновленный магазин

--- buttons.py
+++ buttons.py
@@ -1,11 +1,14 @@
 import disnake
 from disnake.enums import ButtonStyle
 
-class Giveaway(disnake.ui.View): #https://docs.disnake.dev/en/stable/api.html?highlight=disnake+ui+view#disnake.ui.View
+
+class Giveaway(
+    disnake.ui.View):  # https://docs.disnake.dev/en/stable/api.html?highlight=disnake+ui+view#disnake.ui.View
     def __init__(self):
         super().__init__(timeout=None)
 
-    @disnake.ui.button(label="Участвовать", custom_id="participate", style=ButtonStyle.primary, emoji='🎁') # вот все про кнопки https://docs.disnake.dev/en/stable/api.html?highlight=disnake+ui+button#disnake.ui.button
+    @disnake.ui.button(label="Участвовать", custom_id="participate", style=ButtonStyle.primary,
+                       emoji='🎁')  # вот все про кнопки https://docs.disnake.dev/en/stable/api.html?highlight=disnake+ui+button#disnake.ui.button
     async def participate_button(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
         return
 
@@ -31,6 +34,7 @@ class GoldRow(disnake.ui.ActionRow):
 
 class RenameModal(disnake.ui.Modal):
     """A Modal window with Text Inputs for renaming server user to the standard patter of [Rank] Nick (Name)"""
+
     def __init__(self, title: str):
         components = [
             disnake.ui.TextInput(
@@ -57,8 +61,10 @@ class RenameModal(disnake.ui.Modal):
         ]
         super(RenameModal, self).__init__(title=title, components=components)
 
+
 class StickyNoteModal(disnake.ui.Modal):
     """A Modal window with Text Input to make a sticky text note as a last message in chat"""
+
     def __init__(self, title="Закрепление сообщения, как последнего в чате"):
         components = [disnake.ui.TextInput(
             label="Сообщение для закрепления в чате",
@@ -66,3 +72,84 @@ class StickyNoteModal(disnake.ui.Modal):
             custom_id="text"
         )]
         super(StickyNoteModal, self).__init__(title=title, components=components)
+
+
+# Новый вид магазина в виде Embed-а с кнопками
+class ShopView(disnake.ui.ActionRow):
+    def __init__(self):
+        super().__init__()
+
+        # -------------------- Кнопка "просмотреть магазин" ------------------------
+        class ButtonView(disnake.ui.button):
+            def __init__(self):
+                super().__init__(
+                    style=disnake.ButtonStyle.primary,
+                    label="Открыть магазин",
+                    custom_id='shop_open'
+                )
+
+        # -------------------- Кнопка "Управление магазином" ------------------------
+        class ButtonAdmin(disnake.ui.button):
+            def __init__(self, disabled:bool):
+                super().__init__(
+                    style=disnake.ButtonStyle.gray,
+                    custom_id='shop_admin',
+                    emoji="⚙️",
+                    disabled=disabled
+                )
+
+        class ButtonPrev(disnake.ui.button):
+            def __init__(self, disabled: bool, emoji="⬅️"):
+                super().__init__(
+                    emoji=emoji,
+                    disabled=disabled,
+                    custom_id='shop_prev'
+                )
+
+            async def callback(self, inter: disnake.MessageInteraction):
+                view = self.view
+                try:
+                    if view.author_id:
+                        if inter.author.id != view.author_id:
+                            return await inter.send("Меню может пользоваться только тот кто отправил команду", ephemeral=True)
+                        else:
+                            # Найти, как связать коллбеки с эмбедом
+                            await inter.response.edit_message()
+                except:
+                    await inter.send('Невозможно переключить страницу', ephemeral=True)
+
+        class ButtonNext(disnake.ui.button):
+            def __init__(self, disabled: bool, emoji="➡️"):
+                super().__init__(
+                    emoji=emoji,
+                    disabled=disabled,
+                    custom_id='shop_next'
+                )
+
+            async def callback(self, inter: disnake.MessageInteraction):
+                view = self.view
+                try:
+                    if view.author_id:
+                        if inter.author.id != view.author_id:
+                            return await inter.send("Меню может пользоваться только тот кто отправил команду", ephemeral=True)
+                        else:
+                            # Найти, как связать коллбеки с эмбедом
+                            await inter.response.edit_message()
+                except:
+                    await inter.send('Невозможно переключить страницу', ephemeral=True)
+
+        class ButtonBuy(disnake.ui.button):
+            def __init__(self, disabled: bool, emoji="🛒"):
+                super().__init__(
+                    emoji=emoji,
+                    disabled=disabled,
+                    custom_id='shop_buy'
+                )
+
+            def callback(self, inter: disnake.MessageInteraction):
+                pass
+
+        self.append_item(ButtonPrev(disabled=True))
+        self.append_item(ButtonNext(disabled=False))
+        self.append_item(ButtonBuy(disabled=False))
+        self.append_item(ButtonAdmin(disabled=True))
