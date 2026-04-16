60d78d6cad3a52dc54387f703a75ffb800c5e775
Visceros <propok63@gmail.com>
2026-04-16 14:14:42 +0300

    Дебаг данных получаемых курсором для витрины магазина

--- Cog_utils.py
+++ Cog_utils.py
@@ -842,9 +842,7 @@ class Shop(commands.Cog):
                 async with db.transaction() as transac:
                 # ОШИБКА! КУРСОР МОЖЕТ СУЩЕСТВОВАТЬ ТОЛЬКО ВНУТРИ ТРАНЗАКЦИИ. НАПИСАЛ ВЫШЕ ТРАНЗАКЦИЮ. ПРОВЕРИТЬ СИНТАКСИС И СТРУКТУРУ.
                     cur = await db.cursor('DECLARE shop_cursor SCROLL CURSOR WITH HOLD FOR SELECT (product_id, product_type, name, price, duration) FROM shop ORDER BY product_id ASC;')
-                    print(f'{type(cur)},\n{cur}')
-                    async for record in cur:
-                        print(record)
+                    await cur.forward(on_page)
                     page = await cur.fetch(on_page)
                     print(f'{type(page)}\n{page}')
                     # Добавляем инфу в сообщение
