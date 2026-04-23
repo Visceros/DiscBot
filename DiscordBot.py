538738de2ce6e886df5011d211c77b4e3502bc77
Visceros <propok63@gmail.com>
2026-04-23 17:48:13 +0300

    Тест работы проверки активности в чате. Обработка ошибки HTTPException в получении истории чата и исключение ботов.

--- DiscordBot.py
+++ DiscordBot.py
@@ -280,24 +280,6 @@ async def _increment_money(server: disnake.Guild):
         #channel_groups_to_account_contain = ['party', 'пати', 'связь', 'voice'] # Count voice activity for money only in specific channels
         for member in server.members:
             if not member.bot:
-                chat_history:list = await member.history(after=datetime.datetime.now(tz=tz)-datetime.timedelta(minutes=1)).flatten()
-                try:
-                    m = chat_history[0]
-                    print(m.author, f'sent {len(chat_history)} messages in', m.channel)
-                except (IndexError, disnake.HTTPException) as e:
-                    print(e.__str__())
-                    # try:
-                    #     gold = await db.fetchval('SELECT gold FROM discord_users WHERE id=$1;', member.id)
-                    #     if gold is not None:
-                    #         gold = int(gold) + 1
-                    #         await db.execute(f'UPDATE discord_users SET gold=$1 WHERE id=$2;', gold, member.id)
-                    #     else:
-                    #         gold = 1
-                    #         await db.execute(f'UPDATE discord_users SET gold=$1 WHERE id=$2;', gold, member.id)
-                    # except Exception as e:
-                    #     await sys_channel.send(f'Got error trying to give money to user {member}, his gold is {gold}')
-                    #     await sys_channel.send(content=e.__str__())
-
                 if str(member.status) not in ['offline'] and member.voice is not None:
                     #if any(item in member.voice.channel.name.lower() for item in channel_groups_to_account_contain) and not (member.voice.self_mute or member.voice.mute): # Give money only in specific channels
                     if not (member.voice.self_mute or member.voice.mute):
@@ -312,6 +294,26 @@ async def _increment_money(server: disnake.Guild):
                         except Exception as e:
                             await sys_channel.send(f'Got error trying to give money to user {member}, his gold is {gold}')
                             await sys_channel.send(content=e.__str__())
+
+                chat_history:list = await member.history(after=datetime.datetime.now(tz=tz)-datetime.timedelta(minutes=1)).flatten()
+                try:
+                    m = chat_history[0]
+                    print(m.author, f'sent {len(chat_history)} messages in', m.channel)
+                except IndexError:
+                    pass
+                except disnake.HTTPException:
+                    print(e.__str__())
+                    # try:
+                    #     gold = await db.fetchval('SELECT gold FROM discord_users WHERE id=$1;', member.id)
+                    #     if gold is not None:
+                    #         gold = int(gold) + 1
+                    #         await db.execute(f'UPDATE discord_users SET gold=$1 WHERE id=$2;', gold, member.id)
+                    #     else:
+                    #         gold = 1
+                    #         await db.execute(f'UPDATE discord_users SET gold=$1 WHERE id=$2;', gold, member.id)
+                    # except Exception as e:
+                    #     await sys_channel.send(f'Got error trying to give money to user {member}, his gold is {gold}')
+                    #     await sys_channel.send(content=e.__str__())
 
 # Проверяем кто из пользователей в данный момент онлайн и находится в голосовом чате. Начисляем им валюту
 async def accounting():
